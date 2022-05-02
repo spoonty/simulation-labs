3b5a9d842ec58be6a32b134943298ba06dc628ae
Sergey Kachalov <sergey.kachalov.02@gmail.com>
2022-05-02 16:02:02 +0700

    Fifth lab of third module completed

--- lab-5/index.js
+++ lab-5/index.js
@@ -1,16 +1,16 @@
 const X = 10;
 
 const teams = [
-    { team: "Chelsea", lambda: 5, points: 0 },
-    { team: "Liverpool", lambda: 4.8, points: 0 },
-    { team: "Arsenal", lambda: 4.5, points: 0 },
-    { team: "Man City", lambda: 4.1, points: 0 },
-    { team: "Tottenham", lambda: 4, points: 0 },
-    { team: "Man Utd", lambda: 3.9, points: 0 },
-    { team: "West Brom", lambda: 3.6, points: 0 },
-    { team: "Everton", lambda: 3.5, points: 0 },
-    { team: "Southampton", lambda: 3.4, points: 0 },
-    { team: "Bournemouth", lambda: 3, points: 0 },
+    { team: "Chelsea", lambda: 5, points: 0, won: 0, lost: 0 },
+    { team: "Liverpool", lambda: 4.8, points: 0, won: 0, lost: 0 },
+    { team: "Arsenal", lambda: 4.5, points: 0, won: 0, lost: 0 },
+    { team: "Man City", lambda: 4.1, points: 0, won: 0, lost: 0 },
+    { team: "Tottenham", lambda: 4, points: 0, won: 0, lost: 0 },
+    { team: "Man Utd", lambda: 3.9, points: 0, won: 0, lost: 0 },
+    { team: "West Brom", lambda: 3.6, points: 0, won: 0, lost: 0 },
+    { team: "Everton", lambda: 3.5, points: 0, won: 0, lost: 0 },
+    { team: "Southampton", lambda: 3.4, points: 0, won: 0, lost: 0 },
+    { team: "Bournemouth", lambda: 3, points: 0, won: 0, lost: 0 },
 ]
 
 const matches = (
@@ -67,15 +67,67 @@ const playMatches = () => {
 
         if (match.firstTeamGoals === match.secondTeamGoals) {
             match.firstTeam.points++;
+            match.firstTeam.won++;
             match.secondTeam.points++;
+            match.secondTeam.won++;
         }
         else if (match.firstTeamGoals > match.secondTeamGoals) {
             match.firstTeam.points += 3;
+            match.firstTeam.won++;
+            match.secondTeam.lost++;
         }
         else {
             match.secondTeam.points += 3;
+            match.secondTeam.won++;
+            match.firstTeam.lost++;
         }
     }
 }
 
-playMatches();
+const playBtn = document.querySelector('.play-button');
+
+const resultTable = document.querySelector('.result-table-body');
+const resultTableRowTemplate = document.querySelector('.result-table-row');
+
+const matchesTable = document.querySelector('.matches-table-body');
+const matchesTableRowTemplate = document.querySelector('.matches-table-row');
+
+const playActions = () => {
+    while (resultTable.children.length > 1) {
+        resultTable.removeChild(resultTable.lastChild);
+    }
+    while (matchesTable.children.length > 1) {
+        matchesTable.removeChild(matchesTable.lastChild);
+    }
+    teams.forEach(team => team.points = team.won = team.lost = 0);
+
+    playMatches();
+    teams.sort((a, b) => b.points - a.points);
+
+    teams.forEach((team, index) => {
+        const resultTableRowItem = resultTableRowTemplate.cloneNode(true);
+        resultTableRowItem.classList.remove('hidden');
+        resultTableRowItem.children[0].innerHTML = index.toString();
+        resultTableRowItem.children[1].innerHTML = team.team;
+        resultTableRowItem.children[2].innerHTML = team.points;
+        resultTableRowItem.children[3].innerHTML = team.won;
+        resultTableRowItem.children[4].innerHTML = team.lost;
+        resultTable.appendChild(resultTableRowItem);
+    })
+
+    matches.forEach(match => {
+        const matchesTableRowItem = matchesTableRowTemplate.cloneNode(true);
+        matchesTableRowItem.classList.remove('hidden');
+        matchesTableRowItem.children[0].innerHTML = match.firstTeam.team;
+        matchesTableRowItem.children[1].innerHTML = match.firstTeamGoals.toString();
+        matchesTableRowItem.children[2].innerHTML = match.secondTeam.team;
+        matchesTableRowItem.children[3].innerHTML = match.secondTeamGoals.toString();
+        matchesTable.appendChild(matchesTableRowItem);
+    })
+}
+
+playActions();
+
+playBtn.addEventListener("click", () => {
+    playActions();
+})
